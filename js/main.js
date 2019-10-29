d1981eb056b41d522f34b73018b6db461636e333
nastenoksi <43612700+nastenoksi@users.noreply.github.com>
2019-10-29 15:25:01 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,50 @@
+$(function() {
+	$('.works-carousel').owlCarousel({
+	    loop:true,
+	    margin:0,
+	    nav:true,
+	    dots: true,
+	    navText: ['<img src="svg/left.svg">', '<img src="svg/right.svg">'],
+	    startPosition: 1,
+	    responsive:{
+	        0:{
+	            items:1
+	        },
+	        600:{
+	            items:1
+	        },
+	        1000:{
+	            items:1
+	        }
+	    }
+	});
+
+	$('.contributor-carousel').owlCarousel({
+	    loop:true,
+	    margin:0,
+	    nav:true,
+	    dots: true,
+	    navText: ['<img src="svg/left.svg">', '<img src="svg/right.svg">'],
+	    // startPosition: 1,
+	    responsive:{
+	        0:{
+	            items:1
+	        },
+	        600:{
+	            items:1
+	        },
+	        1000:{
+	            items:1
+	        }
+	    }
+	});
+
+
+	$('ul.tabs__caption').on('click', 'li:not(.active)', function(e) {
+		e.preventDefault();
+    $(this)
+      .addClass('active').siblings().removeClass('active')
+      .closest('section.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
+  });
+
+});
