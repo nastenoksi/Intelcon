35cb5166ebc30a1604eb9753cb3f2c27931207ae
nastenoksi <43612700+nastenoksi@users.noreply.github.com>
2019-11-18 12:57:46 +0300

    Add files via upload

---
+++ send.php
@@ -0,0 +1,32 @@
+<?
+if((isset($_POST['email'])&&$_POST['email']!="")){
+        $to = 'свой email';
+        $subject = 'Присоединиться';
+        if (isset($_POST['about_yuorself'])&&$_POST['about_yuorself']!="") {
+         $about_yuorself = '<p>О себе: '.$_POST['about_yuorself'].'</p>';
+        }
+
+        if (isset($_POST['name'])&&$_POST['name']!="") {
+         $name = '<p>Имя: '.$_POST['name'].'</p>';
+        }
+
+        if (isset($_POST['phone'])&&$_POST['phone']!="") {
+            $phone = '<p>Телефон: '.$_POST['phone'].'</p>';
+        }
+        
+        $message = '
+                <html>
+                    <body>  
+                     ' . $name . '
+                        <p>Email: '.$_POST['email'].'</p>'
+                        . $phone . 
+                        . $about_yuorself . '
+     </body>
+                </html>'; 
+        $headers = "MIME-Version: 1.0" . PHP_EOL .
+        "Content-Type: text/html; charset=utf-8" . PHP_EOL .
+        'From: Intelcon' . PHP_EOL .
+        'Reply-To: Intelcon' . PHP_EOL;
+        mail(simanova_a@mail.ru, $subject, $message, $headers);
+}
+?>
